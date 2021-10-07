1fdbec392a853525fb079b97809e6894d795e247
Mike Pirog <mike@lando.dev>
2021-10-07 10:47:34 -0400

    #0: Scaffold test stuff part 21

--- scripts/psh-mysql-helper.sh
+++ scripts/psh-mysql-helper.sh
@@ -2,18 +2,33 @@
 set -e
 exec 2>&1
 
-cat /etc/mysql/my.cnf || true
-cp -rf /etc/platform/templates/my.cnf /tmp/my.cnf
-chown app:app /tmp/my.cnf
-
+chown app:app /run/.configs/etc/platform/templates/my.cnf
+chmod 644 /run/.configs/etc/platform/templates/my.cnf
+id
+cat /etc/passwd
 
 if [ ! -f /mnt/data/.mysql-ready ]; then
     echo "database install"
     chpst -u app:app mysql_install_db \
-          --defaults-file=/tmp/my.cnf \
+          --defaults-file=/etc/platform/templates/my.cnf \
+          --datadir=/mnt/data/mysql \
+          --force \
+          --skip-name-resolve || true
+
+    chpst -u app:app mysql_install_db \
+          # --defaults-file=/etc/platform/templates/my.cnf \
+          --datadir=/mnt/data/mysql \
+          --force \
+          --skip-name-resolve || true
+
+    chpst -u app:app mysql_install_db \
+          # --defaults-file=/etc/platform/templates/my.cnf \
+          --no-defaults \
           --datadir=/mnt/data/mysql \
           --force \
           --skip-name-resolve
+
+
      touch /mnt/data/.mysql-ready
 else
     # In case of UID instability, and to provide an upgrade path to the app user
@@ -22,7 +37,7 @@ fi
 
 echo "mysqld launch"
 exec chpst -u app:app /usr/sbin/mysqld \
-    --defaults-file=/tmp/my.cnf \
+    --defaults-file=/etc/platform/templates/my.cnf \
     --basedir=/usr \
     --datadir=/mnt/data/mysql \
     --plugin-dir=/usr/lib/mysql/plugin \
